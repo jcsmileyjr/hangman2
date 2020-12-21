8d88b321ca5752fad7624d9d4604e56420e95091
Smiley <jcsmileyjr@gmail.com>
2020-12-21 17:56:00 -0600

    Refactor to use random phrases

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ export class AppComponent {
   timer: number = 180;
   interval: any; // varible used to stop the Interval timer
   wordsToGuess: string[] = ["Sister", "Snow", "Family","Brother", "Friend", "School", "Daddy", "Mommy"];
-  currentProblem: number = 0; // IMPORTANT: Currently 11 length max or update design responsiveness
+  currentProblem: string; // IMPORTANT: Currently 11 length max or update design responsiveness
   currentGuessedLetters: string[] = [];
   displayArray: string[] = [];
   gameLives: number[] = [1,2,3,4,5];
@@ -19,27 +19,27 @@ export class AppComponent {
 
   constructor() {
     this.interval = setInterval(()=> this.countdown(), 1000);
-    this.createDisplayedProblem(this.wordsToGuess[ this.currentProblem]);
+    this.createDisplayedProblem(this.getRandomPhrase());
   }
 
   // Method passed to App-Input component to get the letter the user inputed and apply to the word problem
   getLetter($event) {
     if(!this.currentGuessedLetters.includes($event)){
-      this.updateScore($event, this.wordsToGuess[ this.currentProblem]); // Update the score
+      this.updateScore($event, this.currentProblem); // Update the score
       this.currentGuessedLetters.push($event); // Track the current guessed letters
-      this.createDisplayedProblem(this.wordsToGuess[ this.currentProblem]); // Display the current phrase with or with letters    
-      this.checkWinConditions(this.wordsToGuess[ this.currentProblem]); // Check if the game has been won.
+      this.createDisplayedProblem(this.currentProblem); // Display the current phrase with or with letters    
+      this.checkWinConditions(); // Check if the game has been won.
     }    
   }
 
   getAnswer($event){
-    let officialAnswer:string = this.wordsToGuess[ this.currentProblem];
+    let officialAnswer:string = this.currentProblem;
     let formattedAnswer:string = $event;
     if(officialAnswer.toLocaleLowerCase() === formattedAnswer.toLocaleLowerCase()){
       this.displayArray = officialAnswer.split("");
       this.score += 60; // Extra 60 points
       this.timer += 60 // extra 60 seconds
-      this.checkWinConditions(this.wordsToGuess[ this.currentProblem]); // Check if the game has been won.
+      this.checkWinConditions(); // Check if the game has been won.
     }else{
       if(this.score >= 60){
         this.score -= 60;
@@ -87,10 +87,9 @@ export class AppComponent {
   }
 
   // Add points for getting all the correct letters in the phrased and start next game
-  checkWinConditions(originalPhrase: string){
+  checkWinConditions(){
     if(!this.displayArray.includes("_")){
       this.score += 100;
-      this.currentProblem += 1;
       this.setupNextGame();
       this.gameLives.push(this.gameLives.length + 1); // Get a heart for answer
       this.timer = this.timer + 60; // Gain a minute
@@ -101,7 +100,7 @@ export class AppComponent {
   // method to reset the game after a player gets all the correct letters of the phrase
   setupNextGame(){
     this.currentGuessedLetters = [];
-    this.createDisplayedProblem(this.wordsToGuess[ this.currentProblem]);
+    this.createDisplayedProblem(this.getRandomPhrase());
   }
 
   // Method used in the toolbox section restart button to restart
@@ -109,14 +108,14 @@ export class AppComponent {
     this.gameLives = [1,2,3,4,5];
     this.score = 0;
     this.currentGuessedLetters = [];
-    this.currentProblem = 0;
-    this.createDisplayedProblem(this.wordsToGuess[ this.currentProblem]);
+    this.createDisplayedProblem(this.getRandomPhrase());
     this.timer = 180;
     clearInterval(this.interval); 
     this.interval = setInterval(()=> this.countdown(), 1000); // reset the timer
     this.gameOver= false;
   }
 
+  // Method to allow the user to buy time with their points
   buyTime($event){
     if(this.score >= 20){
       this.score -= 20;
@@ -124,8 +123,18 @@ export class AppComponent {
     }
   }
 
+  // Method to switch between user inputing one charater or one word
   answerPhrase($event){
     this.answerPhrasePopup = true;
+  }
+
+  // Method to get a random word from the array of phrases
+  getRandomPhrase(){
+    const randomNumber = Math.floor(Math.random() * this.wordsToGuess.length);
+    let currentWord = this.wordsToGuess[randomNumber];
+    this.wordsToGuess.splice(randomNumber,1);
+    this.currentProblem = currentWord;
+    return currentWord;
   }
 
 }

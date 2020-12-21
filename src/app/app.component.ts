ba60536fc2188ad5e5386ef1937f31b4645ae5d3
Smiley <jcsmileyjr@gmail.com>
2020-12-21 08:42:01 -0600

    Restyle design responsiveness for problem and hearts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   score: number = 0;
   timer: number = 90;
-  problem: string = "Celebration";
+  problem: string = "Celebration"; // IMPORTANT: Currently 11 length max or update design responsiveness
   currentGuessedLetters: string[] = ["e", "a", "c", "n", "b", "t","l"];
   displayArray: string[] = [];
   lives: number[] = [1,2,3,4,5];

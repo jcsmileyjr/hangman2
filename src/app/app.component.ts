889bd8bcf2a9d2199f99ee976501b33b2e2fa0cc
Smiley <jcsmileyjr@gmail.com>
2020-12-21 13:49:03 -0600

    Refactor to update the score based on correct letters and phrased

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,7 @@ export class AppComponent {
   timer: number = 180;
   interval; // varible used to stop the Interval timer
   problem: string = "Celebration"; // IMPORTANT: Currently 11 length max or update design responsiveness
+  numberOfCorrectLetters: number = 0;
   currentGuessedLetters: string[] = [];
   displayArray: string[] = [];
   lives: number[] = [1,2,3,4,5];
@@ -20,10 +21,12 @@ export class AppComponent {
     this.createDisplayedProblem(this.problem);
   }
 
-  // Method to pass to App-Input component to get the letter the user inputed and apply to the word problem
+  // Method passed to App-Input component to get the letter the user inputed and apply to the word problem
   getLetter($event) {
-    this.currentGuessedLetters.push($event);
-    this.createDisplayedProblem(this.problem);
+    this.updateScore($event, this.problem); // Update the score
+    this.currentGuessedLetters.push($event); // Track the current guessed letters
+    this.createDisplayedProblem(this.problem); // Display the current phrase with or with letters    
+    this.checkWinConditions(this.problem); // Check if the game has been won.
   }
 
   // Countdown timer that ends game
@@ -44,6 +47,25 @@ export class AppComponent {
       }else{
         return "_";
       }
-    })    
+    })
+
   }
+
+  // Update the score by checking if the inputted letter is in the phrased and not already guessed. Also tracks the number of correct answers
+  updateScore(letter: string ,phrase: string){
+    let phraseArray = phrase.toLowerCase().split("");
+    const updatedLetter = letter.toLowerCase();
+    if(phraseArray.includes(updatedLetter) && !this.currentGuessedLetters.includes(updatedLetter)){
+      this.score += 10;
+      this.numberOfCorrectLetters += 1;
+    }
+  }
+
+  // Add points for getting all the correct letters in the phrased. 
+  checkWinConditions(originalPhrase: string ){
+    if(originalPhrase.length === this.numberOfCorrectLetters){
+      this.score += 100;
+    }
+  }
+
 }

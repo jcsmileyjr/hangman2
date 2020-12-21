8c2af9a7d8916300b54927baa4b8d590e5bba8de
Smiley <jcsmileyjr@gmail.com>
2020-12-21 10:41:40 -0600

    Refactor timer to add countdown functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   score: number = 0;
-  timer: number = 90;
+  timer: number = 180;
+  interval;
   problem: string = "Celebration"; // IMPORTANT: Currently 11 length max or update design responsiveness
   currentGuessedLetters: string[] = ["e", "a", "c", "n", "b", "t","l"];
   displayArray: string[] = [];
   lives: number[] = [1,2,3,4,5];
+  gameOver: boolean = false;
 
   constructor() {
+    this.interval = setInterval(()=> this.countdown(), 1000);
     this.createDisplayedProblem(this.problem);
+  }
+
+  // Countdown timer that ends game
+  countdown(){
+    this.timer = this.timer - 1;
+    if(this.timer <= 0){
+      this.gameOver = true;
+      clearInterval(this.interval);
+    }
   }
 
 
